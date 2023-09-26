8e4addfff784f0aa611efeff83cf5be216cff90f
jgcarrasco <jorgarcar97@gmail.com>
2023-09-26 11:07:50 +0200

    a

--- evaluation.py
+++ evaluation.py
@@ -11,6 +11,7 @@ from dataset import *
 torch.set_grad_enabled(False)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Device: {device}")
 
 print("Loading the model...")
 model = HookedTransformer.from_pretrained(
